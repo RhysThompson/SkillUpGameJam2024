3e27aaf0d9d56a95066daab9b86c93977b424ba4
RhysThompson <45787691+RhysThompson@users.noreply.github.com>
2024-06-07 02:23:20 +0100

    Fixed laser not extending out of sight when not hitting anything.

--- Assets/Scripts/LaserScript.cs
+++ Assets/Scripts/LaserScript.cs
@@ -5,10 +5,8 @@ public class LaserScript : MonoBehaviour
     public GameObject Sparks;
     void FixedUpdate()
     {
-        if (Physics.Raycast(transform.position, -this.transform.up, out Hit, Mathf.Infinity, 1))
-        {
-            this.transform.localScale = new Vector3(1, Hit.distance, 1);
-            Sparks.transform.position = Hit.point;
-        }
+        bool hitTarget = (Physics.Raycast(transform.position, -this.transform.up, out Hit, Mathf.Infinity, 1));
+        this.transform.localScale = new Vector3(1, hitTarget ? Hit.distance : 2000, 1);
+        Sparks.transform.position = hitTarget ? Hit.point : new Vector3(0,-2000,0);
     }
 }
