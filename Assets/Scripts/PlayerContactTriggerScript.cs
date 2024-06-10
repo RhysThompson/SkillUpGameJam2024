f41f92dc21d3eaa676060ee185d7e768acb0463b
RhysThompson <45787691+RhysThompson@users.noreply.github.com>
2024-06-10 19:18:02 +0100

    Added the first level of set 2 Altered PlayerContactTriggerScript so that it is easier to trigger it's event from an animation event

--- Assets/Scripts/PlayerContactTriggerScript.cs
+++ Assets/Scripts/PlayerContactTriggerScript.cs
@@ -5,6 +5,6 @@ public class PlayerContactTriggerScript : MonoBehaviour
     public UnityEngine.Events.UnityEvent FunctiontoCall;
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) FunctiontoCall.Invoke();
+        if (other == null || other.CompareTag("Player")) FunctiontoCall.Invoke(); // other will equal null when this is called from an animation
     }
 }
